9343b8bf93c006c1b6255caeb5f50cff874af4e9
Orxan Qaragozlu <orkhanrg@code.edu.az>
2023-03-03 19:56:39 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,405 @@
+﻿using System;
+
+namespace Tapsiriq2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //l1:
+            //    string a;
+            //    int number;
+            //    Console.Write("Daxil ediden eded: ");
+
+            //    a = Console.ReadLine();
+
+            //    if (!int.TryParse(a, out number))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
+
+            //    if (number < 1000 || number >= 10000)
+            //    {
+            //        Console.WriteLine("4 reqemli eded deyil");
+            //        goto l1;
+            //    }
+            //    Console.WriteLine(number);
+            //    int sum = 0;
+
+            //    while (number > 0)
+            //    {
+            //        int qaliq = number % 10;
+            //        sum += qaliq;
+            //        number = number / 10;
+
+            //    }
+            //    Console.WriteLine(sum);
+            #endregion
+
+            #region Task 2
+            //l1:
+            //    int number;
+            //    string a;
+            //    Console.Write("ededi daxil et: ");
+            //    a = Console.ReadLine();
+
+            //    if (!int.TryParse(a, out number))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+
+            //    }
+            //    if (number < 100000 || number >= 1000000)
+            //    {
+            //        Console.WriteLine("6 reqemli eded daxil et: ");
+            //        goto l1;
+            //    }
+            //    Console.WriteLine(number);
+
+            //    int sum = 0;
+            //    while (number > 0)
+            //    {
+            //        int firstThree = number / 1000;
+            //        int qaliq = firstThree % 10;
+            //        sum += qaliq;
+            //        number /= 10;
+
+            //    }
+            //    Console.WriteLine(sum);
+            #endregion
+
+            #region Task 3
+            //l1:
+            //    string a;
+            //    int number;
+            //    Console.Write("Daxil ediden eded: ");
+
+            //    a = Console.ReadLine();
+
+            //    if (!int.TryParse(a, out number))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
+
+            //    if (number < 100000000 || number >= 1000000000)
+            //    {
+            //        Console.WriteLine("9 reqemli eded deyil");
+            //        goto l1;
+            //    }
+            //    Console.WriteLine(number);
+
+            //    int sum = 0;    
+            //    while (number > 0)
+            //    {
+            //        int firstSix = number / 1000;
+            //        int middleThree = firstSix % 1000;
+            //        int qaliq = middleThree % 10;
+            //        sum += qaliq;
+            //        number /= 10;
+            //    }
+            //    Console.WriteLine(sum);
+            #endregion
+
+            #region Task 4
+            //l1:
+            //    string a;
+            //    int number;
+            //    Console.Write("Daxil ediden eded: ");
+
+            //    a = Console.ReadLine();
+
+            //    if (!int.TryParse(a, out number))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
+
+            //    if (number < 10000 || number >= 100000)
+            //    {
+            //        Console.WriteLine("5 reqemli eded deyil");
+            //        goto l1;
+            //    }
+            //    Console.WriteLine(number);
+            //    int sum = 0;
+
+            //    while (number > 0)
+            //    {
+            //        int firstNumber = number / 10000;
+            //        int lastNumber = number % 10;
+            //        sum = firstNumber + lastNumber;
+            //        break;
+            //    }
+            //    Console.WriteLine(sum * sum);
+            #endregion
+
+            #region Task 5
+            //l1:
+            //    string a;
+            //    int number;
+            //    Console.Write("Daxil ediden eded: ");
+
+            //    a = Console.ReadLine();
+
+            //    if (!int.TryParse(a, out number))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
+
+            //    if (number < 100000 || number >= 1000000)
+            //    {
+            //        Console.WriteLine("6 reqemli eded deyil");
+            //        goto l1;
+            //    }
+            //    Console.WriteLine(number);
+            //    int sum = 0;
+
+            //    while (number > 0)
+            //    {
+            //        int firstNumber = number / 100000;
+            //        sum =  number * 10 + firstNumber;
+            //        break;
+            //    }
+            //    Console.WriteLine(sum);
+            #endregion
+
+            #region Task 6
+            //l1:
+            //    string a;
+            //    int number;
+            //    Console.Write("Daxil ediden eded: ");
+
+            //    a = Console.ReadLine();
+
+            //    if (!int.TryParse(a, out number))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
+
+            //    if (number < 10000000 || number >= 100000000)
+            //    {
+            //        Console.WriteLine("8 reqemli eded deyil");
+            //        goto l1;
+            //    }
+            //    Console.WriteLine(number);
+            //    int result = 0;
+            //    while (number > 0)
+            //    {
+            //        result = (number / 10) % 1000000;
+            //        break;
+
+            //    }
+            //    Console.WriteLine(result);
+
+            #endregion
+
+            #region Task 7
+
+            //l1:
+            //    string a;
+            //    int number;
+            //    Console.Write("Daxil ediden eded: ");
+
+            //    a = Console.ReadLine();
+
+            //    if (!int.TryParse(a, out number))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
+
+            //    if (number < 1000 || number >= 10000)
+            //    {
+            //        Console.WriteLine("7 reqemli eded deyil");
+            //        goto l1;
+            //    }
+            //    Console.WriteLine(number);
+            //    int newNumber = 0;
+            //    int qaliq = 0;
+            //    int addNumber = 0; 
+
+            //    while (number > 0)
+            //    {
+            //        qaliq = number % 10;
+            //        newNumber = newNumber * 10 + qaliq;
+            //        number= number / 10;
+            //        addNumber = (80000 + newNumber) * 10 + 8; 
+
+            //    }
+            //    Console.WriteLine(addNumber);
+            #endregion
+
+            #region Task 8
+
+            //l1:
+            //    string a;
+            //    int number;
+            //    Console.Write("Daxil ediden eded: ");
+
+            //    a = Console.ReadLine();
+
+            //    if (!int.TryParse(a, out number))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
+
+            //    if (number < 10000 || number >= 100000)
+            //    {
+            //        Console.WriteLine("5 reqemli eded deyil");
+            //        goto l1;
+            //    }
+            //    Console.WriteLine(number);
+
+
+            //int sum = 0;
+            //while (number > 0)
+            //{
+            //   int thirdNumber = (number % 1000) / 100;
+            //   int lastNumber = number % 10; 
+            //   sum = lastNumber + thirdNumber;
+            //    break;
+
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region Task 9
+
+            //    int a;
+            //    int qaliq = 0;
+            //    int number = 0;
+            //l1:
+
+            //    Console.Write("Daxil ediden eded: ");
+
+            //    if (!int.TryParse(Console.ReadLine(), out a))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
+
+            //    if (a < 10000000 || a >= 1000000000)
+            //    {
+            //        Console.WriteLine("9 reqemli eded deyil");
+            //        goto l1;
+            //    }
+
+            //    Console.WriteLine(a);
+
+
+            //    while (a > 0)
+            //    {
+            //        qaliq = a % 10;
+            //        a /= 100;
+            //        number = number * 10 + qaliq;
+
+            //    }
+
+            //    while (number > 0)
+            //    {
+            //        qaliq = number % 10;
+            //        number /= 10;
+
+            //        Console.Write(qaliq);
+            //    }
+            #endregion
+
+            #region Task 10
+            //    int a;
+            //    int qaliq = 0;
+            //    int number = 0;
+            //l1:
+
+            //    Console.Write("Daxil ediden eded: ");
+
+            //    if (!int.TryParse(Console.ReadLine(), out a))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
+
+            //    if (a < 100000000 || a >= 1000000000)
+            //    {
+            //        Console.WriteLine("9 reqemli eded deyil");
+            //        goto l1;
+            //    }
+            //    //Console.WriteLine(a);
+
+            //    while (a > 0)
+            //    {
+            //        qaliq = a % 10;
+            //        a /= 100;
+            //        number = number * 10 + qaliq;
+
+            //    }
+            //    while (number > 0)
+            //    {
+            //        qaliq = number % 10;
+            //        number /= 10;
+
+            //        Console.Write(qaliq);
+            //    }
+
+
+            #endregion
+
+            #region Task 11
+        //    int a;
+        //l1:
+        //    Console.Write("Daxil ediden eded: ");
+
+        //    if (!int.TryParse(Console.ReadLine(), out a))
+        //    {
+        //        Console.WriteLine("eded daxil edilmeyib");
+        //        goto l1;
+        //    }
+
+        //    if (a < 10000000 || a >= 100000000)
+        //    {
+        //        Console.WriteLine("8 reqemli eded deyil");
+        //        goto l1;
+        //    }
+        //    Console.WriteLine(a);
+        //    int sum = 0;
+        //    int number = 0;
+        //    double result = 0;
+        //    while (a > 0)
+        //    {
+        //        number = a % 100;
+        //        sum += number;
+        //        a /= 100;
+        //        result = sum * 100 + 99;
+        //        result =- (result * 0.18);
+        //    }
+        //    Console.WriteLine(result);
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+        
+   
