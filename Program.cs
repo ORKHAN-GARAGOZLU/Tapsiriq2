62af01a4f0c1638fbf331163352c2311ed9f4428
ORKHAN-GARAGOZLU <orkhanrg@code.edu.az>
2023-03-26 22:08:48 +0400

    add

--- Program.cs
+++ Program.cs
@@ -349,53 +349,427 @@ namespace Tapsiriq2
             #endregion
 
             #region Task 11
-        //    int a;
-        //l1:
-        //    Console.Write("Daxil ediden eded: ");
+            //    int a;
+            //l1:
+            //    Console.Write("Daxil ediden eded: ");
 
-        //    if (!int.TryParse(Console.ReadLine(), out a))
-        //    {
-        //        Console.WriteLine("eded daxil edilmeyib");
-        //        goto l1;
-        //    }
+            //    if (!int.TryParse(Console.ReadLine(), out a))
+            //    {
+            //        Console.WriteLine("eded daxil edilmeyib");
+            //        goto l1;
+            //    }
 
-        //    if (a < 10000000 || a >= 100000000)
-        //    {
-        //        Console.WriteLine("8 reqemli eded deyil");
-        //        goto l1;
-        //    }
-        //    Console.WriteLine(a);
-        //    int sum = 0;
-        //    int number = 0;
-        //    double result = 0;
-        //    while (a > 0)
-        //    {
-        //        number = a % 100;
-        //        sum += number;
-        //        a /= 100;
-        //        result = sum * 100 + 99;
-        //        result =- (result * 0.18);
-        //    }
-        //    Console.WriteLine(result);
+            //    if (a < 10000000 || a >= 100000000)
+            //    {
+            //        Console.WriteLine("8 reqemli eded deyil");
+            //        goto l1;
+            //    }
+            //    Console.WriteLine(a);
+            //    int sum = 0;
+            //    int number = 0;
+            //    double result = 0;
+            //    while (a > 0)
+            //    {
+            //        number = a % 100;
+            //        sum += number;
+            //        a /= 100;
+            //        result = sum * 100 + 99;
+            //        result =- (result * 0.18);
+            //    }
+            //    Console.WriteLine(result);
             #endregion
 
+            #region Task 12
+            //l1:
+            //    Console.WriteLine("1-ci 5 reqemli ededi daxil edin");
+            //    string strvalue=Console.ReadLine();
+            //    bool tru = int.TryParse(strvalue, out int value);
+            //    int say = (int)Math.Log10(value) + 1;
+            //    if (!tru)
+            //    {
+            //        goto l1;
+
+            //    }
+            //    if (say != 5)
+            //    {
+            //        goto l1;
+            //    }
+            //    int sum = 0;
+            //    int resudie2 = value % 10;
+            //    while (value>0)
+            //    {
+            //        int resudie = value % 10;
+            //        value /= 10;
+            //        sum += resudie;
+
+            //    }
+
+
+            //l2:
+            //    Console.WriteLine("2-ci 5 reqemli ededi daxil edin");
+            //    string strvalue2 = Console.ReadLine();
+            //    bool tru2 = int.TryParse(strvalue2, out int value2);
+            //    int say2 = (int)Math.Log10(value2) + 1;
+            //    if (!tru2)
+            //    {
+            //        goto l2;
+
+            //    }
+            //    if (say != 5)
+            //    {
+            //        goto l2;
+            //    }
+            //    int sum2 = 1;
+            //    while (value2 > 0)
+            //    {
+            //        int resudie = value2 % 10;
+            //        value2 /= 10;
+            //        sum2 = sum2 * resudie;
+
+            //    }
+
+
+            //    int final = (sum + sum2) * 10 + resudie2;
+            //    Console.WriteLine(final); ;
+            #endregion
+
+            #region Task 13
+
+            //l1:
+            //    Console.WriteLine("1-ci 5 reqemli ededi daxil edin");
+            //    string strvalue = Console.ReadLine();
+            //    bool tru = int.TryParse(strvalue, out int value);
+            //    int say = (int)Math.Log10(value) + 1;
+            //    if (!tru)
+            //    {
+            //        goto l1;
+
+            //    }
+            //    if (say != 5)
+            //    {
+            //        goto l1;
+            //    }
+            //    int sum=0;
+            //    int sum2 = 0;
+
+            //    while (value>0)
+            //    {
+            //        int resudie1 = value % 10;
+            //        value = value / 10000;
+            //        sum = sum*10+ resudie1;
+
+
+            //    }
+            //    while (sum > 0)
+            //    {
+            //        int resudie2=sum % 10;
+            //        sum /= 10;
+            //        sum2 = sum2 * 10 + resudie2;
+
+            //    }
+
+            //    Console.WriteLine(sum2); 
+            //l2:
+
+            //    Console.WriteLine("2-ci 5 reqemli ededi daxil edin");
+            //    string strvalue2 = Console.ReadLine();
+            //    bool tru2 = int.TryParse(strvalue2, out int value2);
+            //    int say2 = (int)Math.Log10(value2) + 1;
+            //    if (!tru2)
+            //    {
+            //        goto l2;
+
+            //    }
+            //    if (say2 != 5)
+            //    {
+            //        goto l2;
+            //    }
+            //    int sum3 = 0;
+            //    int sum4 = 0;
+
+            //    while (value2 > 0)
+            //    {
+            //        int resudie3 = value2 % 10;
+            //        value2 = value2 / 10000;
+            //        sum3 = sum3 * 10 + resudie3;
+
+
+            //    }
+            //    while (sum3 > 0)
+            //    {
+            //        int resudie4 = sum3 % 10;
+            //        sum3/= 10;
+            //        sum4 = sum4 * 10 + resudie4;
+
+            //    }
+
+            //    Console.WriteLine(sum4);
+            //l3:
+            //    Console.WriteLine("3-cu 5 reqemli ededi daxil edin");
+            //    string strvalue3 = Console.ReadLine();
+            //    bool tru3 = int.TryParse(strvalue3, out int value3);
+            //    int say3 = (int)Math.Log10(value3) + 1;
+            //    if (!tru3)
+            //    {
+            //        goto l3;
+
+            //    }
+            //    if (say3 != 5)
+            //    {
+            //        goto l3;
+            //    }
+            //    int sum5 = 0;
+            //    int sum6 = 0;
+
+            //    while (value3 > 0)
+            //    {
+            //        int resudie4 = value3 % 10;
+            //        value3 = value3 / 10000;
+            //        sum5 = sum5 * 10 + resudie4;
+
+
+            //    }
+            //    while (sum5 > 0)
+            //    {
+            //        int resudie5 = sum5 % 10;
+            //        sum5 /= 10;
+            //        sum6 = sum6 * 10 + resudie5;
+
+            //    }
+
+            //    Console.WriteLine(sum6);
+            //    int cem = sum6 + sum4 + sum2;
+            //    double faiz = (double)cem / 2;
+            //    double final = faiz + cem;
+            //    Console.WriteLine(final);
+            #endregion
+
+            #region Task 14
+            //l1:
+            //    Console.WriteLine("1-ci 6 reqemli ededi daxil edin");
+            //    string strvalue=Console.ReadLine();
+            //    bool tru=int.TryParse(strvalue, out int value);
+            //    int say=(int)Math.Log10((int)value)+1;
+            //    if (!tru)
+            //    {
+            //        goto l1;
+            //    }
+            //    if (say!=6)
+            //    {
+            //        goto l1;
+            //    }
+
+            //    int azvalue = value / 1000;
+
+            //    Console.WriteLine(azvalue);
+
+
+            //l2:
+            //    Console.WriteLine("2-ci 6 reqemli ededi daxil edin");
+            //    string strvalue2 = Console.ReadLine();
+            //    bool tru2 = int.TryParse(strvalue2, out int value2);
+            //    int say2 = (int)Math.Log10((int)value2) + 1;
+            //    if (!tru2)
+            //    {
+            //        goto l2;
+            //    }
+            //    if (say2 != 6)
+            //    {
+            //        goto l2;
+            //    }
+            //    int azvalue2 = value2 / 1000;
+            //    Console.WriteLine(azvalue2);
+            //l3:
+            //    Console.WriteLine("3-cu 6 reqemli ededi daxil edin");
+            //    string strvalue3 = Console.ReadLine();
+            //    bool tru3 = int.TryParse(strvalue3, out int value3);
+            //    int say3 = (int)Math.Log10((int)value3) + 1;
+            //    if (!tru3)
+            //    {
+            //        goto l3;
+            //    }
+            //    if (say != 6)
+            //    {
+            //        goto l3;
+            //    }
+            //    int azvalue3 = value3 / 1000;
+            //    int azvaule4 = azvalue + azvalue2 + azvalue3;
+            //    Console.WriteLine(azvalue3);
+            //l4:
+            //    Console.WriteLine("4-cu 7 reqemli ededi daxil edin");
+            //    string strvalue4 = Console.ReadLine();
+            //    bool tru4 = int.TryParse(strvalue4, out int value4);
+            //    int say4 = (int)Math.Log10((int)value4)+1;
+            //    if (!tru4)
+            //    {
+            //        goto l4;
+            //    }
+            //    if (say4 != 7)
+            //    {
+            //        goto l4;
+            //    }
+            //    int sonvalue = value4 % 10000;
+            //    int valuecem = azvaule4 + sonvalue;
+
+            //    int ilkvalue4 = value4 / 10000;
+            //    int sum = 1;
+            //    while (ilkvalue4>0)
+            //    {
+            //        int resudie = ilkvalue4 % 10;
+            //        ilkvalue4/=10;
+            //        sum = sum * resudie;
+            //    }
+            //    int son = valuecem - sum;
+            //    int resudie60 = son * 60 /100;
+            //    int artir60 = resudie60 * 100 + 60;
+            //    int faiz18 = artir60 * 18 / 100;
+            //    int final = artir60 - faiz18;
+            //    Console.WriteLine(final);
+
+            #endregion
+
+            #region Task 15
+            //l1:
+            //    Console.WriteLine("1-ci 3 reqemli ededi daxil edin");
+            //    string strvalue = Console.ReadLine();
+            //    bool tru = int.TryParse(strvalue, out int value);
+            //    int say = (int)Math.Log10((int)value) + 1;
+            //    if (!tru)
+            //    {
+            //        goto l1;
+            //    }
+            //    if (say != 3)
+            //    {
+            //        goto l1;
+            //    }
+
+
+
+            //l2:
+            //    Console.WriteLine("2-ci 3 reqemli ededi daxil edin");
+            //    string strvalue2 = Console.ReadLine();
+            //    bool tru2 = int.TryParse(strvalue2, out int value2);
+            //    int say2 = (int)Math.Log10((int)value2) + 1;
+            //    if (!tru2)
+            //    {
+            //        goto l2;
+            //    }
+            //    if (say2 != 3)
+            //    {
+            //        goto l2;
+            //    }
+
+            //l3:
+            //    Console.WriteLine("3-cu 6 reqemli ededi daxil edin");
+            //    string strvalue3 = Console.ReadLine();
+            //    bool tru3 = int.TryParse(strvalue3, out int value3);
+            //    int say3 = (int)Math.Log10((int)value3) + 1;
+            //    if (!tru3)
+            //    {
+            //        goto l3;
+            //    }
+            //    if (say3 != 6)
+            //    {
+            //        goto l3;
+            //    }
+
+            //l4:
+            //    Console.WriteLine("4-cu 6 reqemli ededi daxil edin");
+            //    string strvalue4 = Console.ReadLine();
+            //    bool tru4 = int.TryParse(strvalue4, out int value4);
+            //    int say4 = (int)Math.Log10((int)value4) + 1;
+            //    if (!tru4)
+            //    {
+            //        goto l4;
+            //    }
+            //    if (say4 != 6)
+            //    {
+            //        goto l4;
+            //    }
+            //l5:
+            //    Console.WriteLine("5-ci 7 reqemli ededi daxil edin");
+            //    string strvalue5 = Console.ReadLine();
+            //    bool tru5 = int.TryParse(strvalue5, out int value5);
+            //    int say5 = (int)Math.Log10((int)value5) + 1;
+            //    if (!tru5)
+            //    {
+            //        goto l5;
+            //    }
+            //    if (say5 != 7)
+            //    {
+            //        goto l5;
+            //    }
+            //    int value5_7 = value5;
+            //    int sum2 = 0;
+            //    while (value5_7 > 0)
+            //    {
+            //        int requide7 = value5_7 % 10;
+            //        value5_7 /= 100;
+            //        sum2 = sum2 * 10 + requide7;
+
+
+
+            //    }
+            //    int tekyer7 = 0;
+            //    while (sum2 > 0)
+            //    {
+            //        int requide = sum2 % 10;
+            //        sum2 /= 10;
+            //        tekyer7 = tekyer7 * 10 + requide;
+            //    }
+
+            //    Console.WriteLine(tekyer7);
+            //    int value3cem = value + value2;
+
+            //    int requide3r = value3cem % 100;
+
+            //    int kvadrat = requide3r * requide3r;
+
+            //    int value3birlesdirme = value * 1000 + value2;
+
+            //    int cem1 = value3birlesdirme + kvadrat;
+
+            //    int value7son5 = value5 % 100000;
+
+            //    int ferq = cem1 - value7son5;
+
+            //    int value6cem = value3 + value4;
+
+            //    int requide6r = value6cem % 1000;
+
+            //    int cem2 = requide6r + ferq;
+
+            //    int sum = 0;
+            //    int sumters = 0;
+            //    while (value5 > 0)
+            //    {
+            //        int requide7 = value5 % 10;
+            //        value5 /= 10;
+            //        sum += requide7;
+
+            //    }
+            //    while (sum > 0)
+            //    {
+            //        int requide7_1 = sum % 10;
+            //        sum /= 10;
+            //        sumters = sumters * 10 + requide7_1;
+            //    }
+
+            //    int cem3 = cem2 + sumters;
+            //    int cem11hasil = cem3 * 100 + 11;
+
+            //    int ferq11_7 = cem11hasil - tekyer7;
+            //    int requidefinal = ferq11_7 % 10;
+            //    int elave8 = ferq11_7 / 10 * 1000 + 880  + requidefinal;
+
+            //    Console.WriteLine(elave8);
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+            #endregion
 
         }
 
